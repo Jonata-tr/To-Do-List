08485978ca476fce5277021e94286c5f6ead7e6d
Jonatan <Jonatatrindasilva@gmail.com>
2023-10-30 19:27:14 -0400

    Startint to ajust the modal activation.

--- script/main.js
+++ script/main.js
@@ -1,5 +1,13 @@
 lucide.createIcons();
 
+
+const filter = document.querySelector("#filter")
+filter.addEventListener("click", ()=> {
+  const filterArea = document.querySelector(".task-filter")
+
+  filterArea.classList.toggle('ativo')
+})
+
 // Prototipo de criação de elementos da toDo List 
 
 const creatTask = document.querySelector(".create-task");
@@ -41,22 +49,64 @@ function createTask(taskInput, taskType){
   
   
 
-  const taskArea = document.querySelector(".to-do-area")
+  const taskArea = document.getElementById(li.task-state)
 
-  console.log(new Date());
   taskArea.appendChild(li.element)
 }
 
 // List Constructor
-function NewListItem(tittle, type, time){
-  this.element = document.createElement('li')
-  this.tittle = `<h3>${tittle}</h3>`
-  this.type = `<p>${type}</p>`
-  this.time = `<span>${time}</span>`
+function NewListItem(state, tittle, type, time){
+  this.element = document.createElement('div');
+  this.element.classList.add('task')
+  this.type = type;
 
-  this.element.innerHTML = `${this.tittle}
-  ${this.type} ${this.time}` 
+  this.element.innerHTML = `
+    <span class="type">${this.type}</span>
+    <h2>${tittle}</h2>
+    <div class="task-type-area">
+      <p>${state}</p>
+      
+      <div>
+        <button>
+          <svg class='icons' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pencil"><path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z"/><path d="m15 5 4 4"/></svg>
+        </button>
+
+        <button>
+          <svg class='icons' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash-2"><path d="M3 6h18"/><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/><line x1="10" x2="10" y1="11" y2="17"/><line x1="14" x2="14" y1="11" y2="17"/></svg>
+        </button>
+        
+        <button>
+          <svg class='icons' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-clock-4"><circle cx="12" cy="12" r="10"/><polyline points="12 6 12 12 16 14"/></svg>
+        </button>
+      </div>
+    </div>  
+  `
 }
+
+const planejada = document.querySelector("#planejada")
+const andamento = document.querySelector("#andamento")
+const completa = document.querySelector("#completa")
+
+const tipos = [
+  'Trabalho',
+  'Casa',
+  'Lazer',
+  'Jogos',
+  'Estudos'
+]
+
+for(let i =0; i <= 4; i++){
+  const li = new NewListItem('Planejada', 'Limpar a casa e depois lavar a louça', tipos[i], '20/03')
+  const lii = new NewListItem('Em andamento', 'Limpar a casa e depois lavar a louça', tipos[i], '20/03')
+  const liii = new NewListItem('Completada', 'Limpar a casa e depois lavar a louça', tipos[i], '20/03')
+
+  planejada.appendChild(li.element)
+  andamento.appendChild(lii.element)
+  completa.appendChild(liii.element)
+}
+
+//##############################################
+// Only one at a time
 
 // ############################################
 // MODAL AREA
@@ -78,7 +128,6 @@ function handleSelect(){
 
 function closeSelect(custom, evento) {
   const buttonView = custom.querySelector(".options-view-button");
-  console.log(buttonView);
 
   const mouseOrTouch = 
   evento == 'mouse' ||
